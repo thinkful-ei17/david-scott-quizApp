5c5f3fa324afd9c68bbc7db5cc383aec7a3a9cb9
David Park <dysp92@gmail.com>
2017-12-11 09:30:52 -0800

    started js

--- index.js
+++ index.js
@@ -1,10 +1,68 @@
-title page{
-    title: "quiz ap",
+// views: start, questions, feedback, lastQuestionFeedback, results
+
+const QUESTIONS = [];
+
+const STORE {
+    view: start,
+    title: start,
+    currentQuestion: null,
+    currentScore: null,
     button: start,
-    question[number]: null,
+    showFeedback: false,
+}
+
+initial question STORE {
+    view: questions,
+    title: null,
+    currentQuestion: question[i],
+    currentScore: fn() {
+        correct / total;
+    },
+    button: submit,
+    showFeedback: false
+}
+
+feedback STORE {
+    view: feedback,
+    title: null,
+    currentQuestion: question[i],
+    currentScore: fn() {
+        correct / total;
+    },
+    button: next question,
+    showFeedback: true,
+}
+
+lastQuestion feedback STORE {
+    view: lastQuestionFeedback,
+    title: null,
+    currentQuestion: question[i],
+    currentScore: fn()
+    button: results,
+    showFeedback: true,
+}
+
+results STORE {
+    view: results,
+    title: results (congrats..),
+    currentQuestion: null,
+    currentScore: fn()
+    button: start again?,
+    showFeedback: false,
+}
+
+
+
+
+
+
+
+function renderQuizPage() {
 
 }
 
-question page{
-    form: choices,
-}
+// make functions for each view
+
+
+
+
