7571fadcfa62dde93675f0e4e02c1587bc2443b2
David Park <dysp92@gmail.com>
2017-12-12 07:00:13 -0800

    new branch

--- index.js
+++ index.js
@@ -22,82 +22,35 @@ const QUESTIONS = [
 
 const store = {
   view: 'start',
-  currentQuestion: null,
-  currentScore: null,
-  button: 'start',
-  showFeedback: false,
+  currentQuestion: 0,
+  currentScore: 0,
+  userAnswer: $('input[type="radio"]:checked').val(),
 };
 
-// initial question STORE {
-//     view: questions,
-//     title: null,
-//     currentQuestion: question[i],
-//     currentScore: fn() {
-//         correct / total;
-//     },
-//     button: submit,
-//     showFeedback: false
-// }
-
-// feedback STORE {
-//     view: feedback,
-//     title: null,
-//     currentQuestion: question[i],
-//     currentScore: fn() {
-//         correct / total;
-//     },
-//     button: next question,
-//     showFeedback: true,
-// }
-
-// lastQuestion feedback STORE {
-//     view: lastQuestionFeedback,
-//     title: null,
-//     currentQuestion: question[i],
-//     currentScore: fn()
-//     button: results,
-//     showFeedback: true,
-// }
-
-// results STORE {
-//     view: results,
-//     title: results (congrats..),
-//     currentQuestion: null,
-//     currentScore: fn()
-//     button: start again?,
-//     showFeedback: false,
-// }
-
 function renderStart() {
-  // render title and button
-  return `
-    <header class="title">
-        <h1>Welcome to our Quiz Page</h1>
+  return `<header class="title">
+    <h1>Welcome to our Quiz Page</h1>
     </header>
     <button class='start-button'>Start Quiz</button>`;
 }
 
 function handleStartButtonClick(){
-  $('form').submit(function(event){
+  $('form').on('click', '.start-button', event => {
     event.preventDefault();
     console.log('this should start things');
     store.view = 'questions';
-    store.currentQuestion = 0;
     renderPage();
-  }
-  );
+  });
 }
 
 function renderQuestion() {
   // question text
   // answers as radio buttons
   return generateQuestion(store.currentQuestion);
-  // generateButton('submit', 'submit');
 }
 
 function renderFeedback() {
   generateQuestion(store.currentQuestion);
-  generateFeedback(store.currentQuestion);
   generateButton('next-Question', 'Next Question');
 }
 
@@ -110,45 +63,51 @@ function generateButton(buttonClass, text) {
 }
 
 function generateQuestion(i=0) {
-  return `<div class="answer-choice">${QUESTIONS[0].question}</div>
+  return `<div class="answer-choice">${QUESTIONS[i].question}</div>
     <input type="radio" name="choices" class="choice-1" id='choice-1'>
-        <label for="choice-1">${QUESTIONS[0].answers[0]}</label>
+        <label for="choice-1">${QUESTIONS[i].answers[0]}</label>
     <input type="radio" name="choices" class="choice-2" id='choice-2'>
-        <label for="choice-2">${QUESTIONS[0].answers[1]}</label>
+        <label for="choice-2">${QUESTIONS[i].answers[1]}</label>
     <input type="radio" name="choices" class="choice-3" id='choice-3'>
-        <label for="choice-3">${QUESTIONS[0].answers[2]}</label>
+        <label for="choice-3">${QUESTIONS[i].answers[2]}</label>
     <input type="radio" name="choices" class="choice-4" id='choice-4'>
-        <label for="choice-4">${QUESTIONS[0].answers[3]}</label>
-    </div>`;
+        <label for="choice-4">${QUESTIONS[i].answers[3]}</label>
+    </div>
+    <button class="submit">submit</button>
+    <button class="next-question">Next question</button>`;
+}
+
+function handleNextQuestion() {
+  $('form').on('click', '.next-question', event => {
+    console.log('clicked next question');
+    store.currentQuestion++;
+    renderPage();
+  });
 }
 
 function handleAnswerClicked(i) {
   $('form').on('click', '.submit', event => {
-    if($('input[type=radio]:checked') === QUESTIONS[i].correctAnswer) {
-      return '<p>Correct!</p>';
-    } else if($('input[type=radio]:checked') !== QUESTIONS[i].correctAnswer) {
-      return '<p>Wrong!</p>';
+    event.preventDefault();
+    if(store.userAnswer === QUESTIONS[i].correctAnswer) {
+      $('form').append('<p>Correct!</p>');
+      store.correctAnswer++;
+    } else {
+      $('form').append(`<p>Wrong! ${store.userAnswer}</p>`);
     }
   });
 }
 
-function generateFeedback(i) {
-  
-  return `<div class="feedback">
-            <h2 class="result">feedback: correct/wrong</h2>
-            <h2 class="correct-answer">results: this one was right</h2>
-          </div>`;
-}
 
-function handleSubmit() {
-  $('.submit-button').submit(event => {
-    store.view = 'feedback';
-  });
-}
 
+
+function handleEventListeners() {
+  handleStartButtonClick();
+  handleAnswerClicked(store.currentQuestion);
+  handleNextQuestion();
+
+}
 
 function renderPage(){
-
   if(store.view === 'start') {
     $('form').html(renderStart());
   }
@@ -169,8 +128,10 @@ function renderPage(){
 
 $(function(){
   renderPage();
-  handleStartButtonClick();
-}
-);
+  handleEventListeners();
+});
 
 // make functions for each view
+
+
+// 
